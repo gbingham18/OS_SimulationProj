b4979e3ad6f51af3e310ef7caf2b1cb0ccaab4d6
Justice Martinez <amartinez18@my.whitworth.edu>
2018-04-09 06:51:22 -0700

    Ran analysis
    Graphs and Data are in the excel files, I have made graphs for response time, wait time, turnaround time and throughput

---
+++ fileInput.h
@@ -0,0 +1,44 @@
+#ifndef FILEINPUT_H_
+#define FILEINPUT_H_
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <queue>
+#include <sstream>
+#include "PCB.h"
+using namespace std;
+
+vector<PCB> readFile(vector<PCB> processes) {
+	ifstream myfile("processes.txt");
+	if (!myfile.is_open())
+	{
+		cout << "File not open" << endl;
+	}
+	string line;
+	vector<int> PCBproperties;
+	vector<int> eventList;
+	while (processes.size() < 1000)
+	{
+		getline(myfile, line);
+		istringstream iss(line);
+		for (string s; iss >> s;)
+		{
+			stringstream geek(s);
+			int x = 0;
+			geek >> x;
+			PCBproperties.push_back(x);
+		}
+		for (int i = 3; i < PCBproperties.size(); i++)
+		{
+			eventList.push_back(PCBproperties[i]);
+		}
+		PCB process(PCBproperties[0], PCBproperties[1], PCBproperties[2], eventList);
+		processes.push_back(process);
+		PCBproperties.clear();
+		eventList.clear();
+	}
+	return processes;
+};
+
+#endif
